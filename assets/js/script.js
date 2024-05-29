3819f9a991ae5544e3d15225dc927256d8723b93
Instructor <acooper@instructors.2u.com>
2024-05-29 18:14:09 -0500

    completed form and dynamic element creation for in class demo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const submitBtn = document.querySelector('#submit');
 const modeBtn = document.querySelector('#mode');
+const contentInput = document.querySelector('#content');
 const titleInput = document.querySelector('#title');
+const userNameInput = document.querySelector('#username');
 const postsEl = document.querySelector('#posts');
 const htmlEl = document.querySelector('html');
 
@@ -19,13 +21,16 @@ const renderPosts = function() {
     const headerEl = document.createElement('header');
     const blogPostEl = document.createElement('article');
     const pEl = document.createElement('p');
+    const footerEl = document.createElement('footer');
 
     headerEl.textContent = post.title; 
-    pEl.textContent = "some content to be added later";
+    pEl.textContent = post.content;
+    footerEl.textContent = post.username;
 
     blogPostEl.appendChild(headerEl);
     blogPostEl.appendChild(pEl);
-    postsEl.appendChild(blogPostEl);
+    blogPostEl.appendChild(footerEl);
+    postsEl.prepend(blogPostEl);
   }
 }
 
@@ -48,11 +53,15 @@ const handleSubmit = function(event) {
   const posts = JSON.parse(data) || [];
 
   // Retrives the title input's value and trims unecessary whitespace 
+  const content = contentInput.value.trim();
   const title = titleInput.value.trim();
+  const username = userNameInput.value.trim();
   
   // Creates a post object
   const post = {
-    title: title
+    content: content,
+    title: title,
+    username: username,
   };
 
   // Append the post object to the posts collection
